34e82e3224201b66b55552cab4a31d86a1804a6e
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-06-02 01:56:40 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,16 +1,18 @@
 import os
 from flask import Flask, flash, request, redirect, send_file, render_template,url_for
 from werkzeug.utils import secure_filename
-
+from best_win_hack_wow_super_pro_max import main_danila
 UPLOAD_FOLDER = os.path.abspath('DATA2')
-ALLOWED_EXTENSIONS = {'txt', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'mp4'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 config = []
 
-
+path_contecst = r"C:\Users\pl1ght\PycharmProjects\RZD_hack"
+path_models = r"C:\Users\pl1ght\PycharmProjects\RZD_hack\models\\"
+name_video = r'C:\Users\pl1ght\PycharmProjects\RZD_hack\DATA2\\'
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -32,7 +34,12 @@ def upload_file():
             filename = secure_filename(file.filename)
 
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            config.append(filename)
+            file_path = os.path.realpath(__file__)
+
+            # Получение директории, в которой находится файл скрипта
+            res = main_danila(path_contecst , path_models,name_video+filename)
+            config.append(res)
+            print(res['value'][0]) # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             return redirect(url_for("return_pb"))
     return render_template('main_page.html')
 
@@ -40,7 +47,7 @@ def upload_file():
 
 @app.route('/return_pb',methods=['get','post'])
 def return_pb():
-    return render_template('result.html')
+    return render_template('result.html',change=config[0])
 #короче редирект после получения материала на
 
 
@@ -49,4 +56,5 @@ def return_pb():
 
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run(host='127.0.0.1', port=12345, debug=False)
